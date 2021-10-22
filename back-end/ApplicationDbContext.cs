0cc8d5184b12d1171a375f7145bf5fe0b2bde589
Elmer1992 <41174392+Elmer1992@users.noreply.github.com>
2021-10-22 16:38:42 -0600

    438 horas

--- back-end/ApplicationDbContext.cs
+++ back-end/ApplicationDbContext.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using back_end.Entidades;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace back_end
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
